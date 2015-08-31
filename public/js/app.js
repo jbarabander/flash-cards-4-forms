58b57b11e80864715c792cd99dc9b3e4cab74b5a
Justin Barabander <jbarabander@gmail.com>
2015-08-31 14:14:18 -0400

    Finished basic states

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,44 @@
-var app = angular.module('flashCards', []);
+var app = angular.module('flashCards', ['ui.router']);
 
 app.value('currentFlashCards', []);
+
+app.config(function($stateProvider) {
+  $stateProvider
+  .state('stats', {
+    url: '/stats',
+    templateUrl: '../stateViews/stats.html',
+    controller: 'StatsController'
+  })
+  .state('newCard', {
+    url: '/newCard',
+    templateUrl: '../stateViews/newCard.html',
+    controller: 'NewCardController'
+  })
+  .state('viewCards', {
+    url: '/viewCards',
+    templateUrl: '../stateViews/viewCards.html',
+    controller: 'MainController'
+  });
+});
+
+
+// app.config(function($stateProvider) {
+//   $stateProvider
+//   .state('home', {
+//     url: '/',
+//     views: {
+//       stats: {
+//         templateUrl: '../stateViews/stats.html',
+//         controller: 'StatsController'
+//       },
+//       newCard: {
+//         templateUrl: '../stateViews/newCard.html',
+//         controller: 'NewCardController'
+//       },
+//       viewCards: {
+//         templateUrl: '../stateViews/viewCards.html',
+//         controller: 'MainController'
+//       }
+//     }
+//   });
+// });
